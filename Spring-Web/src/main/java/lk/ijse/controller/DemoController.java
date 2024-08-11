2316c19d45e608d12c77a2e512e83f777f09bf78
maleeshaLH <lhmv114@gmil.com>
2024-08-11 15:16:02 +0530

    PathVariable Annotation

--- Spring-Web/src/main/java/lk/ijse/controller/DemoController.java
+++ Spring-Web/src/main/java/lk/ijse/controller/DemoController.java
@@ -2,15 +2,16 @@ package lk.ijse.controller;
 
 import jakarta.servlet.http.HttpServlet;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/demo")
 public class DemoController{
-    @GetMapping(value = "/healt?")
-    public String healthCheck(){
-        return "DemoController run Prefectiy";
+    @GetMapping(value = "/health/{name}")
+    public String healthCheck(@PathVariable("name")String name){
+        return "DemoController run Prefectiy  :"+name;
 
     }
 
